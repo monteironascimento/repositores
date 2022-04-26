787f2cb07064cea13b5be2293b6fecfcabdb0193
Rodrigo Monteiro do Nascimento <rodrigo.monteiro.nascimento@outlook.com>
2022-04-26 14:46:39 -0300

    init

---
+++ src/main/java/br/com/monteiro/repositores/RepositoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.monteiro.repositores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/monteiro/repositores/RepositoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.monteiro.repositores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
