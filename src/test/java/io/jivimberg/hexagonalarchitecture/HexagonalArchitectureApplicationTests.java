9d56b792fd1dcc5e5cd024ff37f5966f966d1259
Rodrigo Monteiro do Nascimento <rodrigo.monteiro.nascimento@outlook.com>
2022-05-09 21:50:58 -0300

    adicionado arquitetura hexagonal

--- src/test/java/io/jivimberg/hexagonalarchitecture/HexagonalArchitectureApplicationTests.java
+++ src/test/java/io/jivimberg/hexagonalarchitecture/HexagonalArchitectureApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.monteiro.repositores;
+package io.jivimberg.hexagonalarchitecture;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RepositoresApplicationTests {
+class HexagonalArchitectureApplicationTests {
 
 	@Test
 	void contextLoads() {
